8b2ac03f558d1d92f0df024d7d3a12fc0d958f71
everange <chencoriver@live.cn>
2023-02-05 10:08:32 +0800

    file2mess.py

---
+++ file2mess.py
@@ -0,0 +1,46 @@
+# Filetobitstream and BitstreamtoFile
+# chenkj@ustc.edu.cn
+
+import os
+import libnum
+import base64
+import numpy as np
+
+
+
+def file2mess(mess_path):
+    _, filename = os.path.split(mess_path)                          # 文件名
+    ext = filename.split('.')[-1]                                   # 后缀名
+    ext_bin = libnum.s2b(ext).zfill(32)                             # 二进制后缀名(32位)
+    with open(mess_path, 'rb') as f:
+        file_data = f.read()
+        file_data_b64 = base64.b64encode(file_data)
+    file_data_bin = libnum.s2b(file_data_b64)                       # 二进制消息内容
+    mess_len_bin = libnum.s2b(str(len(file_data_bin))).zfill(160)    # 二进制消息长度
+
+    ext_list = ext_bin.replace('', ' ')[1:-1].split(' ')            # 文件后缀(32位)
+    mess_len_list = mess_len_bin.replace('', ' ')[1:-1].split(' ')  # 消息长度(160位)
+    file_data_list = file_data_bin.replace('', ' ')[1:-1].split(' ')
+
+    mess = np.array(ext_list + mess_len_list + file_data_list).astype(int)
+    return mess
+
+
+def mess2file(mess, save_path='.', save_name='rec'):
+    mess_str = ''
+    for i in range(len(mess)):
+        mess_str = mess_str + str(int(mess[i]))
+    ext_bin = mess_str[:32]
+    mess_len_bin = mess_str[32:192]
+    ext = str(libnum.b2s(ext_bin).decode('utf-8'))
+    mess_len = str(libnum.b2s(mess_len_bin).decode('utf-8'))
+    mess_bin = mess_str[192:192 + int(mess_len)]
+    mess_b64 = libnum.b2s(mess_bin)
+    with open(os.path.join(save_path, save_name + '.' + ext), 'wb') as f:
+        mess = base64.b64decode(mess_b64)
+        f.write(mess)
+    return os.path.join(save_path, save_name + '.' + ext)
+
+mess_path = 'filetransfer/1.tex'
+mess = file2mess(mess_path)
+mess2file(mess)
